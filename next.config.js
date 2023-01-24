933af60788842b515a582153ab2f0049408cccf1
Olive <oliverachermann23@gmail.com>
2023-01-24 21:01:22 +0100

    new buisness brand

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost","m151-medusa-server.s3.eu-central-1.amazonaws.com"],
   },
 })
 
